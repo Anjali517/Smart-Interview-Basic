a25614390154cb55f3bbee578ebc40c740d628e6
Anjali Dongala <dongarianjaliyadavv@gmail.com>
2024-08-22 19:57:56 +0530

    Create Odd even index.py

---
+++ Odd even index.py
@@ -0,0 +1,17 @@
+s=input()
+o=[]
+e=[]
+for i in range(len(s)):
+    if i%2==0:
+        e.append(s[i])
+    else:
+        o.append(s[i])
+#print(o,end="")
+#print(e,end="")
+for i in o:
+    print(i,end="")
+for i in e:
+          print(i,end="")
+          
+          
+        
