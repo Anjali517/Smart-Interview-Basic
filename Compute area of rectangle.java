2e01716b52fe0019b6b892dbee11cc83f8bcbce1
Anjali Dongala <dongarianjaliyadavv@gmail.com>
2024-08-22 19:35:13 +0530

    Create Compute area of rectangle.java

---
+++ Compute area of rectangle.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int a,b;
+        long c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a*(long)b;
+        System.out.println(c);
+    }
+}
