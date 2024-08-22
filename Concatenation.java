1768111bde207e18463c0e37144b90671be9cfcf
Anjali Dongala <dongarianjaliyadavv@gmail.com>
2024-08-22 19:22:11 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int i;
+        int a[]=new int[n+n];
+        for(i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            for(int j)
+        }
+    }
+}
